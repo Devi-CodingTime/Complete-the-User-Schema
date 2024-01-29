923a6af5b98418b9f0d5f379a2db7a569da48337
Devi-CodingTime <devipurnima26899@gmail.com>
2024-01-30 01:04:25 +0530

    schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,22 @@
 var mongoose= require("mongoose");
 
 //Write your schema here with name of schema as userSchema
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+       },
+       email:{
+        type: String,
+        required: true
+       },
+    pswd : {
+        type: String,
+        required: true
+        },
+    role : {
+        type: String
+        }
+});
 
 module.exports =mongoose.model("users",userSchema);
